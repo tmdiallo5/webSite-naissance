1468798eb2cd76aacec43cbab51de07b1d6e7f72
Thierno Maviatou Diallo <tmdiallo5@gmail.com>
2024-11-03 22:49:34 +0100

    les dates ont ete bien formatee

--- website-naissance/src/utils/date.ts
+++ website-naissance/src/utils/date.ts
@@ -1,6 +1,12 @@
+const dayOfWeek = ["Lun.", "Mar.", "Mer.", "Jeu.", "Sam.", "Dim"];
+const MonthOfYear = ["Jan.", "Fev.", "Mars.", "Avril.", "Mai.", "Jui.", "Juil.", "Aout.", "Sept.", "Oct.", "Nov.", "Dec"];
+const formatDay = (day: number) => {
+    return `0${day}`.slice(-2)
+}
+
 const formatDate = (value: string) => {
     const date = new Date(value.split(" ")[0]);
-    return `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`
+    return `${formatDay(date.getDay())} ${MonthOfYear[date.getMonth()]} ${date.getFullYear()}`
 }
 
 export {formatDate};
