c03a97f61590cf31943c4670f3a5ecfa6c28bdf3
Thierno Maviatou Diallo <tmdiallo5@gmail.com>
2024-11-09 21:02:33 +0100

    ici on a cree le formulaire de recherche

--- website-naissance/src/pages/Declaration.tsx
+++ website-naissance/src/pages/Declaration.tsx
@@ -1,20 +1,35 @@
-import { useEffect, useState } from "react";
-import { formatDate, getStatusColor, getStatusColorLabel } from "../utils";
-import { Declarations } from "@/types/Declarations";
 import { useDeclarations } from "@/hooks";
 import DeclarationsItems from "@/components/declarations/DeclarationsItems";
+import { Link } from "react-router-dom";
 
 function Declaration() {
   const { declarations, sortByStatus, sortByDate } = useDeclarations();
 
   return (
-    <div className=" border-4  bg-white shadow-md rounded-md">
-      <DeclarationsItems
-        declarations={declarations}
-        sortByStatus={sortByStatus}
-        sortByDate={sortByDate}
-      />
-    </div>
+    <>
+      <div className=" border-4  bg-white shadow-md rounded-md mb-3 flex justify-between items-center px-3 py-3">
+        <input
+          type="text"
+          name=""
+          id="rechercher"
+          placeholder="Rechercher par nom"
+          className="bg-gray-200 px-3 py-2 rounded-md w-96"
+        />
+        <Link
+          to={"/private/declaration"}
+          className="bg-green-700 rounded-md text-white px-3 py-2"
+        >
+          Nouvelle declaration
+        </Link>
+      </div>
+      <div className=" border-4  bg-white shadow-md rounded-md">
+        <DeclarationsItems
+          declarations={declarations}
+          sortByStatus={sortByStatus}
+          sortByDate={sortByDate}
+        />
+      </div>
+    </>
   );
 }
 
