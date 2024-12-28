d1abdf2a40d3efe289c0c63b823a2d646b5a5c91
Thierno Maviatou Diallo <tmdiallo5@gmail.com>
2024-12-28 17:37:02 +0100

    Nous avons tester la presence d'un mail dans la base

--- ms-naissance/ms-naissance/src/main/java/tech/chillo/ms_naissance/profiles/ProfilesRepository.java
+++ ms-naissance/ms-naissance/src/main/java/tech/chillo/ms_naissance/profiles/ProfilesRepository.java
@@ -2,5 +2,9 @@ package tech.chillo.ms_naissance.profiles;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProfilesRepository extends JpaRepository<Profile, Integer> {
+
+    Optional<Profile> findByEmail(String email);
 }
--- ms-naissance/ms-naissance/src/test/java/tech/chillo/ms_naissance/profiles/ProfilesRepositoryTest.java
+++ ms-naissance/ms-naissance/src/test/java/tech/chillo/ms_naissance/profiles/ProfilesRepositoryTest.java
@@ -1,6 +1,8 @@
 package tech.chillo.ms_naissance.profiles;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
@@ -9,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.util.Assert;
 
 import java.util.List;
+import java.util.Optional;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
@@ -16,12 +19,43 @@ public class ProfilesRepositoryTest {
 
     @Autowired
     ProfilesRepository profilesRepository;
-    @Test
-    void shouldReturnListOfProfiles(){
+
+    @BeforeEach
+    void setUp(){
         // Arrange
         Profile profileOne = Profile.builder().email("one@email.test").build();
         Profile profileTwo = Profile.builder().email("two@email.test").build();
         this.profilesRepository.saveAll(List.of(profileOne, profileTwo));
+
+    }
+    @AfterEach
+    void reset(){
+
+        this.profilesRepository.deleteAll();
+
+    }
+
+    @Test
+    void shouldReturnEmptyProfilesByEmail(){
+        // Act
+        Optional<Profile> profiles = this.profilesRepository.findByEmail("mavi@gmail.com");
+
+        // Assert
+        Assertions.assertTrue(profiles.isEmpty());
+    }
+    @Test
+    void shouldReturnProfilesByEmail(){
+        // Act
+        Optional<Profile> profiles = this.profilesRepository.findByEmail("one@email.test");
+
+        // Assert
+        Assertions.assertTrue(profiles.isPresent());
+    }
+
+
+    @Test
+    void shouldReturnListOfProfiles(){
+
 
         // Act
         List<Profile> profiles = this.profilesRepository.findAll();
