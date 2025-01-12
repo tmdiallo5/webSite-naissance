cab992a8f384b8f09ce0243a74eabef73202a5ff
Thierno Maviatou Diallo <tmdiallo5@gmail.com>
2025-01-12 12:54:01 +0100

    ok

--- ms-naissance/ms-naissance/src/main/java/tech/chillo/ms_naissance/declarations/DeclarationController.java
+++ ms-naissance/ms-naissance/src/main/java/tech/chillo/ms_naissance/declarations/DeclarationController.java
@@ -1,4 +1,24 @@
 package tech.chillo.ms_naissance.declarations;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "declarations")
 public class DeclarationController {
+    private DeclarationService declarationService;
+
+    public DeclarationController(DeclarationService declarationService) {
+        this.declarationService = declarationService;
+    }
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void create(@RequestBody Declaration declaration) {
+        this.declarationService.create(declaration);
+    }
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Declaration> search(@RequestBody Declaration declaration) {
+        return this.declarationService.search();
+    }
 }
--- ms-naissance/ms-naissance/src/main/java/tech/chillo/ms_naissance/declarations/DeclarationService.java
+++ ms-naissance/ms-naissance/src/main/java/tech/chillo/ms_naissance/declarations/DeclarationService.java
@@ -1,6 +1,7 @@
 package tech.chillo.ms_naissance.declarations;
 
 import org.springframework.stereotype.Service;
+import tech.chillo.ms_naissance.profiles.ProfilesService;
 
 import java.util.List;
 
@@ -8,12 +9,15 @@ import java.util.List;
 public class DeclarationService {
 
     private DeclarationRepository declarationRepository;
+    private ProfilesService profilesService;
 
-    public DeclarationService(DeclarationRepository declarationRepository) {
+    public DeclarationService(DeclarationRepository declarationRepository, ProfilesService profilesService) {
         this.declarationRepository = declarationRepository;
+        this.profilesService = profilesService;
     }
 
     public void create(Declaration declaration) {
+
             this.declarationRepository.save(declaration);
     }
 
